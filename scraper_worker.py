9216ced6f2f91d97eaf602a90a907fa06cd5ee00
pcyo <feng79624@gmail.com>
2021-02-07 18:50:56 +0000

    Update scraper log

--- scraper_worker.py
+++ scraper_worker.py
@@ -3,9 +3,12 @@ from services.earnings_service import set_earnings
 import logging
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     scraper = earnings_whispers_scraper.EarningsWhispersScraper()
     earnings_list = scraper.scrape()
+    logging.info('Scrap %d earnings', len(earnings_list))
 
     for earnings in earnings_list:
         set_earnings(earnings)
+
+    logging.info('Scraper exit')
